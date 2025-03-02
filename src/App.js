d8bc83d69a3824c4ef5f82c214c8332d189acb85
Justin Bueno <justinbueno63@gmail.com>
2025-03-01 23:47:05 -0800

    modified:   src/App.js 	new file:   src/Functionality/wordInfo.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {use, useRef, useState}from "react";
+import React, {useRef, useState}from "react";
 import "tachyons"
 import "./App.css"
 import SearchBar from "./SearchBar.js"
@@ -7,10 +7,37 @@ export default function App() {
     const [searchinput, setSearchinput] = useState("");
     const [word, setWord] = useState("Fish")  
     const [wordinfo, setWordinfo] = useState([])
-    const [content, setContent] = useState([wordinfo])
+    // const [content, setContent] = useState([wordinfo])
     const [counter, setCounter] = useState(0)
-
     const inputref = useRef(word);
+    // const [phoenetic,setPhoenetic] = useState("feesh");
+    // const [phonetics,setPhoenetics] = useState([]);
+    // const [meanings, setMeanings] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    function ProcessWordInfo({arrayobject}) {
+        arrayobject.map((item) => {
+            // One Key Pair
+            let word = item.word
+
+            // One Key Pair
+            let phonetic = item.phonetic
+
+            // Multiple Key Pairs (Array of Objects containing values
+            // such as Text, Audio, sourceURL, & License. Ignore License.)
+            let phonetics = item.phonetics
+
+            // Mutliple Key Pairs. Contains array of objects with key pairs such as
+            // partOFSpeech, Definitions. Definitions has it's own array of objects with
+            // values such as definition, synonyms, and antonyms. These values can be undefined.
+            let meanings = item.meanings
+            return (
+                <h2>{item.phonetics}</h2>
+            )
+        })
+    }
+
 
     function inputSearchBox(event) {
         setSearchinput(event.target.value)
@@ -18,6 +45,9 @@ export default function App() {
 
 
     function searchWord (event){
+        if (loading){
+            
+        }
         setWord(searchinput);
         setCounter((counter)=>counter+1)
         if (word) {
@@ -29,14 +59,14 @@ export default function App() {
               }
              return response.json()
           })
-          .then(content => setWordinfo(content)).catch(error => console.error(`Fetch Error: ${error}`))
+          .then(content => {setWordinfo(content);console.log(content)}).catch(error => console.error(`Fetch Error: ${error}`))
           console.log(inputref.current.value)
         }
     }
     function WordOrEntry() {
         // console.log(counter)
-        if (counter>1) {
-            return (<Card wordinfo={content} word={word}></Card>)
+        if (counter>=1) {
+            return (<Card wordinfo={wordinfo} word={word}></Card>)
         } else {
             return (<h2>Please input a word!</h2>)
         }
@@ -47,8 +77,10 @@ export default function App() {
         <div className="tc">
             <h1 className="tc">The Dictionary Site</h1>
             <SearchBar inputref={inputref} searchbarValue = {inputSearchBox} clickFunction={() => searchWord()}/>
-            {/* <Card wordinfo={content} word={word}></Card> */}
-            <WordOrEntry/>
+            {/* <Card wordinfo={wordinfo} word={word}></Card> */}
+            {/* <h2>{wordinfo[0]}</h2> */}
+            <ProcessWordInfo arrayobject={wordinfo}/>
+            {/* <WordOrEntry wordinfo={wordinfo} word={header}/> */}
         </div>
     )
 }
---
+++ src/Functionality/wordInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function WordInfo() {
+    return (
+        <h2>testing</h2>
+    )
+}
