30942a9e6ff43f3cc3b9db76ff8cf8d69b9074e4
Justin Bueno <justinbueno63@gmail.com>
2025-03-02 01:12:31 -0800

    Worked on an input bug that caused site crashing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "tachyons"
 import "./App.css"
 import SearchBar from "./SearchBar.js"
 import Card from "./Components/Card.js";
+import capitalizeFirstLetter from "./Functionality/capitalizeFirstLetter.js";
 export default function App() {
     const [searchinput, setSearchinput] = useState("");
     const [word, setWord] = useState("Fish")  
@@ -14,6 +15,7 @@ export default function App() {
     // const [phonetics,setPhoenetics] = useState([]);
     // const [meanings, setMeanings] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [okResponse, setOkResponse] =useState(null);
 
 
     function ProcessWordInfo({arrayobject}) {
@@ -51,12 +53,14 @@ export default function App() {
             fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputref.current.value}`)
             .then(response => {
               if (!response.ok){
-                  setWordinfo([])
-                  throw new Error(`HTTP ERROR! Status Code: ${response.status}`)
+                setOkResponse(false)
+                setWordinfo([])
+                throw new Error(`HTTP ERROR! Status Code: ${response.status}`)
               }
-             return response.json()
+            setOkResponse(true)
+            return response.json()
           })
-          .then(content => {setWordinfo(content);console.log(content)}).catch(error => console.error(`Fetch Error: ${error}`))
+          .then(content => {setWordinfo(content);console.log(content)}).catch(error => {console.error(`Fetch Error: ${error}`);setOkResponse(false)} )
           console.log(inputref.current.value)
         }
     }
@@ -70,31 +74,35 @@ export default function App() {
 
     function WordOrEntry({wordinfo}) {
         // console.log(counter)
-        if (counter>=1 && loading===false) {
+        let mainWord
+        if (okResponse===true && counter>=1 && loading===false) {
             // return (<Card wordinfo={wordinfo} word={word}></Card>)
             return (
-                wordinfo.map((item) => {
-                // One Key Pair
-                let word = item.word
-    
-                // One Key Pair
-                let phonetic = item.phonetic
-    
-                // Multiple Key Pairs (Array of Objects containing values
-                // such as Text, Audio, sourceURL, & License. Ignore License.)
-                let phonetics = item.phonetics
-    
-                // Mutliple Key Pairs. Contains array of objects with key pairs such as
-                // partOFSpeech, Definitions. Definitions has it's own array of objects with
-                // values such as definition, synonyms, and antonyms. These values can be undefined.
-                let meanings = item.meanings
-                return (
-                    <h2>{item.word}</h2>
-                )
-            }))
-        } else if (counter>=1 && loading==true) {
+                <div>
+                {wordinfo.map((item) => {
+                    // One Key Pair
+                    let word = item.word
+        
+                    // One Key Pair
+                    let phonetic = item.phonetic
+        
+                    // Multiple Key Pairs (Array of Objects containing values
+                    // such as Text, Audio, sourceURL, & License. Ignore License.)
+                    let phonetics = item.phonetics
+        
+                    // Mutliple Key Pairs. Contains array of objects with key pairs such as
+                    // partOFSpeech, Definitions. Definitions has it's own array of objects with
+                    // values such as definition, synonyms, and antonyms. These values can be undefined.
+                    let meanings = item.meanings
+                    return (
+                        <h2>{item.word}</h2>
+                    )
+                })}
+                </div>             
+            )
+        } else if (counter>=1 && loading===true) {
             return <h2>Loading...</h2>
-        }
+        } else if (okResponse === false) return <h2>Requested word is not within the dictionary!</h2>
         else {
             return (<h2>Please input a word!</h2>)
         }
