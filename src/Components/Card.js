a11cfc813ee1832816a144c092cf527d2a0f44db
Justin Bueno <justinbueno63@gmail.com>
2025-03-03 05:33:03 -0800

    Fixed structure of components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,29 +1,15 @@
 import React from "react";
-
-//word={word} phonetic={phonetic} phonetics={phonetics} meanings={meanings} sourceURLs={sourceURLs}
+import AudioControl from "./AudioControl";
+import DefinitionCarousel from "./DefinitionCarousel"
 export default function Card({word,phonetic,audio,meanings,sourceURLs}) {
-    // console.log(audio)
-    // const AudioComponent = memo(function AudioComponent({audio}) {
-    //     return (
-    //         audio&& (
-    //             <audio controls>
-    //             <source src={audio} type="audio/mpeg"/>
-    //             </audio>
-    //         )
-    //     )
-    // },[{audio}])
     return (
             <div>
                 <div>
                     <h2>{word}</h2>
-                    {/* <AudioComponent audio={audio} /> */}
-                    {audio&& (
-                        <audio controls>
-                        <source src={audio} type="audio/mpeg"/>
-                        </audio>
-                    )}
+                    <h3>{phonetic}</h3>
                 </div>
-                <h3>{phonetic}</h3>
+                <AudioControl audio={audio} />
+                <DefinitionCarousel meanings={meanings} />
             </div>
         )
 }
